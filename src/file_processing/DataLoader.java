42568ad6e47077187671d8add8050c9d0139dfd6
Tywin <tiaven1104@gmail.com>
2019-03-28 16:24:08 -0400

    clean up a bit

--- src/file_processing/DataLoader.java
+++ src/file_processing/DataLoader.java
@@ -17,8 +17,8 @@ import graph.Digraph;
 import graph.DirectedEdge;
 import graph.EdgeWeightedDigraph;
 
-public class DataLoader {
-
+public class DataLoader { 
+	
 	// Top 2 cheapest meals from McDonald's, Burger King and Wendy's
 	public static final Meal M1 = new Meal("McChicken - Meal", 3.79);
 	public static final Meal M2 = new Meal("Double Cheeseburger - Meal", 4.38);
@@ -26,7 +26,7 @@ public class DataLoader {
 	public static final Meal B2 = new Meal("Chicken Nuggets (10 Pc.)", 5.89);
 	public static final Meal W1 = new Meal("Dave's Hot 'n Juicy 1/4 lb. Single with Cheese", 6.19);
 	public static final Meal W2 = new Meal("Spicy Chicken - Combo", 6.59);
-
+	 
 	public static ArrayList<City> cities;
 	public static Map<Integer, City> IndexToCity = new HashMap<Integer, City>();
 
@@ -133,72 +133,6 @@ public class DataLoader {
 		return digraph;
 	}
 
-//	public static EdgeWeightedDigraph LoadEdgeWeightedDigraph() throws IOException {
-//		if (DataLoader.cities == null)
-//			try {
-//				loadCities();
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		// Init the Digraph data structure
-//		EdgeWeightedDigraph graph = new EdgeWeightedDigraph(cities.size());
-//		ArrayList<Integer[]> unfinished = new ArrayList<Integer[]>();
-//
-//		String fileIn = "data/connectedCities.txt";
-//		CSVReader reader = new CSVReader(new FileReader(fileIn), ',', '"', 0);
-//		List<String[]> allRows = reader.readAll();
-//		for (String[] row : allRows) {
-//			int index_from = getCityIndexByName(row[0].toLowerCase().trim());
-//			int index_to = getCityIndexByName(row[1].toLowerCase().trim());
-//			City city_from = IndexToCity.get(index_from);
-//			City city_to = IndexToCity.get(index_to);
-//			// If the origin city is BOSTON, can eat anywhere
-//			Meal prev_meal = city_from.getMeal();
-//			Meal selected_meal;
-//			if (index_from == 0) {
-//				selected_meal = M1;
-//				city_to.setMeal(selected_meal);
-//				DirectedEdge edge = new DirectedEdge(index_from, index_to, selected_meal.getPrice());
-//				graph.addEdge(edge);
-//				continue;
-//			}
-//			if (prev_meal != null) {
-//				if (prev_meal.equals(M1))
-//					selected_meal = M2;
-//				else {
-//					selected_meal = M1;
-//				}
-//				city_to.setMeal(selected_meal);
-//				DirectedEdge edge = new DirectedEdge(index_from, index_to, selected_meal.getPrice());
-//				graph.addEdge(edge);
-//			} else {
-//				unfinished.add(new Integer[] { index_from, index_to });
-//			}
-//		}
-
-//		for (ListIterator<Integer[]> pair = unfinished.listIterator(); pair.hasNext();) {
-//			Integer[] value = pair.next();
-//			int index_from = value[0];
-//			int index_to = value[1];
-//			City city_from = IndexToCity.get(index_from);
-//			City city_to = IndexToCity.get(index_to);
-//			Meal prev_meal = city_from.getMeal();
-//			Meal selected_meal;
-//			if (prev_meal != null) {
-//				if (prev_meal.equals(M1))
-//					selected_meal = M2;
-//				else {
-//					selected_meal = M1;
-//				}
-//				city_to.setMeal(selected_meal);
-//				DirectedEdge edge = new DirectedEdge(index_from, index_to, selected_meal.getPrice());
-//				graph.addEdge(edge);
-//			}
-//			pair.remove();
-//		}
-//		return graph;
-//	}
-
 	public static EdgeWeightedDigraph LoadEdgeWeightedDigraph() throws IOException {
 		if (DataLoader.cities == null)
 			try {
@@ -219,14 +153,4 @@ public class DataLoader {
 		}
 		return graph;
 	}
-
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-//		Digraph di = loadDigraphs();
-//		System.out.println(di);
-//		loadCities();
-//		System.out.println(IndexToCity);
-		EdgeWeightedDigraph graph = LoadEdgeWeightedDigraph();
-		System.out.println(graph);
-	}
-
 }
