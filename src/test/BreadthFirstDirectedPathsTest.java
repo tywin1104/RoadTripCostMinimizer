acadc6a3b75908279d0945aeba014a29debe8bbc
Tywin <tiaven1104@gmail.com>
2019-03-28 14:23:14 -0400

    finish basic junit tests for dfs & bfs

--- src/test/BreadthFirstDirectedPathsTest.java
+++ src/test/BreadthFirstDirectedPathsTest.java
@@ -1,19 +1,52 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
+import java.util.Stack;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import file_processing.DataLoader;
+import graph.BreadthFirstDirectedPaths;
+import graph.Digraph;
+
 class BreadthFirstDirectedPathsTest {
+
+	Digraph digraph;
+	int from_index;
+	int to_index;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		digraph = DataLoader.loadDigraphs();
+		from_index = DataLoader.getCityIndexByName("Boston");
+		to_index = DataLoader.getCityIndexByName("Minneapolis");
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	// test there is a route from Boston to Minneapolis
+	void test_reachable() {
+		BreadthFirstDirectedPaths bfs = new BreadthFirstDirectedPaths(digraph, from_index);
+		assertTrue(bfs.hasPathTo(to_index));
 	}
 
+	@Test
+	// test for the correctness of the routes
+	void test_path() {
+		BreadthFirstDirectedPaths bfs = new BreadthFirstDirectedPaths(digraph, from_index);
+		Stack<Integer> path = (Stack<Integer>) bfs.pathTo(to_index);
+		while (!path.empty()) {
+			int v = path.pop();
+			assertTrue(bfs.hasPathTo(v));
+		}
+	}
+
+	@Test
+	// test that all possible routes are examined(all connected cities are examined)
+	void test_route_examination() {
+		BreadthFirstDirectedPaths bfs = new BreadthFirstDirectedPaths(digraph, from_index);
+		for (int i = 0; i < digraph.V(); i++) {
+			assertTrue(bfs.hasPathTo(i));
+		}
+	}
 }
--- src/test/DepthFirstDirectedPathsTest.java
+++ src/test/DepthFirstDirectedPathsTest.java
@@ -1,19 +1,52 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Stack;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import file_processing.DataLoader;
+import graph.DepthFirstDirectedPaths;
+import graph.Digraph;
+
 class DepthFirstDirectedPathsTest {
+	Digraph digraph;
+	int from_index;
+	int to_index;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		digraph = DataLoader.loadDigraphs();
+		from_index = DataLoader.getCityIndexByName("Boston");
+		to_index = DataLoader.getCityIndexByName("Minneapolis");
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	// test there is a route from Boston to Minneapolis
+	void test_reachable() {
+		DepthFirstDirectedPaths dfs = new DepthFirstDirectedPaths(digraph, from_index);
+		assertTrue(dfs.hasPathTo(to_index));
 	}
 
+	@Test
+	// test for the correctness of the routes
+	void test_path() {
+		DepthFirstDirectedPaths dfs = new DepthFirstDirectedPaths(digraph, from_index);
+		Stack<Integer> path = (Stack<Integer>) dfs.pathTo(to_index);
+		while (!path.empty()) {
+			int v = path.pop();
+			assertTrue(dfs.hasPathTo(v));
+		}
+	}
+
+	@Test
+	// test that all possible routes are examined(all connected cities are examined)
+	void test_route_examination() {
+		DepthFirstDirectedPaths dfs = new DepthFirstDirectedPaths(digraph, from_index);
+		for (int i = 0; i < digraph.V(); i++) {
+			assertTrue(dfs.hasPathTo(i));
+		}
+	}
 }
